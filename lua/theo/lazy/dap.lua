692e52c5823b1619ff3b3db412bcad31f537dd00
TheoRhytical <rhyscabonita@gmail.com>
2024-05-29 14:59:31 +0800

    DAP Descriptions & virtual-text
    - Added Descriptions for Search Key feature
    - setup nvim-dap-virtual-text
    - Changed dap keybindings

--- lua/theo/lazy/dap.lua
+++ lua/theo/lazy/dap.lua
@@ -5,6 +5,7 @@ return {
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
 			"leoluz/nvim-dap-go",
+			"theHamsta/nvim-dap-virtual-text",
 		},
 		config = function()
 			local dap = require("dap")
@@ -12,6 +13,8 @@ return {
 
 			require("dap-go").setup()
 			dapui.setup()
+
+			require("nvim-dap-virtual-text").setup()
 
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
@@ -26,29 +29,51 @@ return {
 				dapui.close()
 			end
 
-			vim.keymap.set("n", "<F5>", dap.continue)
-			vim.keymap.set("n", "<F10>", dap.step_over)
-			vim.keymap.set("n", "<F11>", dap.step_into)
-			vim.keymap.set("n", "<F12>", dap.step_out)
-			vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<Leader>B", dap.set_breakpoint)
+			-- vim.keymap.set("n", "<F5>", dap.continue, { desc = "[D]ebug Continue" })
+			-- vim.keymap.set("n", "<F10>", dap.step_over, { desc = "[D]ebug Step Over" })
+			-- vim.keymap.set("n", "<F11>", dap.step_into, { desc = "[D]ebug Step Into" })
+			-- vim.keymap.set("n", "<F12>", dap.step_out, { desc = "[D]ebug Step Out" })
+			-- vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "[D]ebug Toggle Breakpoint" })
+			-- vim.keymap.set("n", "<Leader>B", dap.set_breakpoint, { desc = "[D]ebug Set Breakpoint" })
+
+			vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "[D]ebug [C]ontinue" })
+			vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "[D]ebug Step [O]ver" })
+			vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "[D]ebug Step [I]nto" })
+			vim.keymap.set("n", "<Leader>dx", dap.step_out, { desc = "[D]ebug Step Out (e[X]it)" })
+			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "[D]ebug Toggle [B]reakpoint" })
+			vim.keymap.set("n", "<Leader>dB", dap.set_breakpoint, { desc = "[D]ebug Set [B]reakpoint" })
 			-- vim.keymap.set("n", "<Leader>lp", dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: ")))
-			vim.keymap.set("n", "<Leader>dr", dap.repl.open)
-			vim.keymap.set("n", "<Leader>dl", dap.run_last)
+			vim.keymap.set("n", "<Leader>dr", dap.repl.open, { desc = "[D]ebug Open [R]EPL" })
+			vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "[D]ebug Run [L]ast" })
+
 			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
 				require("dap.ui.widgets").hover()
-			end)
+			end, { desc = "[D]ebug UI Widgets [H]over" })
 			vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
 				require("dap.ui.widgets").preview()
-			end)
+			end, { desc = "[D]ebug UI Widgets [P]review" })
 			vim.keymap.set("n", "<Leader>df", function()
 				local widgets = require("dap.ui.widgets")
 				widgets.centered_float(widgets.frames)
-			end)
+			end, { desc = "[D]ebug UI Widgets Center [F]rames" })
 			vim.keymap.set("n", "<Leader>ds", function()
 				local widgets = require("dap.ui.widgets")
 				widgets.centered_float(widgets.scopes)
-			end)
+			end, { desc = "[D]ebug UI Widgets Center [S]copes" })
+
+			vim.keymap.set(
+				"n",
+				"<Leader>dt",
+				dapui.toggle,
+				-- ":DapUiToggle<CR>",
+				{ noremap = true, desc = "[D]ebug UI [T]oggle" }
+			)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>dr",
+				":lua require('dapui').open({reset = true})<CR>",
+				{ noremap = true, desc = "[D]ebug UI [R]eset" }
+			)
 		end,
 	},
 }
