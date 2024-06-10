04dd97a678108c00ed35e3a886d0bde81b6d13d7
TheoRhytical <rhyscabonita@gmail.com>
2024-06-10 13:18:50 +0800

    JS Debugging config
    - Started configuring dap for JS debugging
    - Still have stuff to fix tho

--- lua/theo/lazy/dap.lua
+++ lua/theo/lazy/dap.lua
@@ -1,3 +1,10 @@
+local js_based_languages = {
+	"typescript",
+	"javascript",
+	"typescriptreact",
+	"javascriptreact",
+	"vue",
+}
 return {
 	{
 		"mfussenegger/nvim-dap",
@@ -5,6 +12,36 @@ return {
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
 			"leoluz/nvim-dap-go",
+			{
+				"microsoft/vscode-js-debug",
+				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+			},
+			{
+				"mxsdev/nvim-dap-vscode-js",
+				config = function()
+					-- JS DAP setup source: https://www.youtube.com/watch?v=Ul_WPhS2bis
+					require("dap-vscode-js").setup({
+						-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+						-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+						debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
+						-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+						-- adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+						adapters = {
+							"chrome",
+							"pwa-node",
+							"pwa-chrome",
+							"pwa-msedge",
+							"pwa-extensionHost",
+							"node-terminal",
+							"node",
+							"js-debug-adapter",
+						}, -- which adapters to register in nvim-dap
+						-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+						-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+						-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+					})
+				end,
+			},
 			"theHamsta/nvim-dap-virtual-text",
 		},
 		config = function()
@@ -13,6 +50,55 @@ return {
 
 			require("dap-go").setup()
 			dapui.setup()
+
+			for _, language in ipairs(js_based_languages) do
+				require("dap").configurations[language] = {
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-chrome",
+						request = "launch",
+						name = 'Start Chrome with "localhost"',
+						url = function()
+							local co = coroutine.running()
+							return coroutine.create(function()
+								vim.ui.input({
+									prompt = "Enter URL: ",
+									default = "http://localhost:3000",
+								}, function(url)
+									if url == nil or url == "" then
+										return
+									else
+										coroutine.resume(co, url)
+									end
+								end)
+							end)
+						end,
+						webRoot = "${workspaceFolder}",
+						skipFiles = { "<node_internals>/**/*.js" },
+						protocol = "inspector",
+						sourceMaps = true,
+						userDataDir = false,
+					},
+					{
+						name = "----- launch.json configs -----",
+						type = "",
+						request = "launch",
+					},
+				}
+			end
 
 			require("nvim-dap-virtual-text").setup()
 
